7ad342faf256c26382c5a71d6cdef0c4946664d1
polesskiy-dev <e34dream@gmail.com>
2018-09-28 16:25:20 +0300

    initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,31 @@
+// .eslintrc.js is prioritised over other eslintrc formats
+
+module.exports = {
+    'env': {
+        'browser': true,
+        'jest': true,
+        'es6': true,
+        'node': true,
+    },
+    'extends': [
+        'airbnb',
+        'prettier',
+    ],
+    'plugins': [
+        'prettier',
+    ],
+    'rules': {
+        'prettier/prettier': ['error', {
+            'singleQuote': true,
+            'trailingComma': 'es5'
+        }],
+        "jsx-a11y/href-no-hash": "off",
+        "react/jsx-one-expression-per-line": "off",
+        "prefer-default-export": "off"
+    },
+    'parserOptions': {
+        'ecmaFeatures': {
+            'jsx': true,
+        }
+    }
+};
---
+++ README.md
@@ -0,0 +1,9 @@
+# Masmovil phone catalog app [![Build Status](https://travis-ci.com/polesskiy-dev/masmovil-phones)](https://travis-ci.com/polesskiy-dev/masmovil-phones)
+
+[See Live demo on heroku]()
+
+# About implementation
+[react-create-app](https://github.com/facebook/create-react-app) for rapid front-end development with some *eslint airbnb config* enhancement.
+
+# Few words about dev features
+* Travis
