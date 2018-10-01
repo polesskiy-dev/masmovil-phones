250413290a3acb277ef42946b43c5b754865ca1d
polesskiy-dev <e34dream@gmail.com>
2018-10-01 13:51:09 +0300

    add Modal from reactstrap

--- client/src/components/containers/PhoneListContainer/PhoneListContainer.jsx
+++ client/src/components/containers/PhoneListContainer/PhoneListContainer.jsx
@@ -1,30 +1,54 @@
-import React, { PureComponent } from 'react';
-import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
-import { compose } from 'redux';
+import React, { Fragment, PureComponent } from "react";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import { compose } from "redux";
 
-import './PhoneListContainer.css';
+import "./PhoneListContainer.css";
 
-import getPhones from '../../../selectors/phones.selector';
-import PhoneCardComponent from '../../presentional/PhoneCardComponent/PhoneCardComponent';
-import withFetchingSpinner from '../../hocs/withFetchingSpinner';
-import withPhonesSubscription from '../../hocs/withPhonesSubscribtion';
+import { Modal, ModalHeader, ModalBody } from "reactstrap";
+import getPhones from "../../../selectors/phones.selector";
+import PhoneCardComponent from "../../presentional/PhoneCardComponent/PhoneCardComponent";
+import withFetchingSpinner from "../../hocs/withFetchingSpinner";
+import withPhonesSubscription from "../../hocs/withPhonesSubscribtion";
 
 class PhoneListContainer extends PureComponent {
   static propTypes = {
-    phones: PropTypes.array.isRequired,
+    phones: PropTypes.array.isRequired
+  };
+
+  state = {
+    modalIsOpen: false
   };
 
   showPhoneDetails = deviceName => {
-    console.log(deviceName)
+    this.setState({
+      modalIsOpen: true,
+      selectedDeviceName: deviceName
+    });
   };
+
+  toggleModal = () => this.setState({ modalIsOpen: !this.state.modalIsOpen });
 
   render() {
     const { phones } = this.props;
+    const { modalIsOpen, selectedDeviceName } = this.state;
 
-    return <section className="phones-list">
-      {phones.map(phone => <PhoneCardComponent key={phone.DeviceName} showPhoneDetails={this.showPhoneDetails} {...phone} />)}
-    </section>;
+    return (<Fragment>
+      <section className="phones-list">
+        {phones.map(phone => <PhoneCardComponent
+          key={phone.DeviceName}
+          showPhoneDetails={this.showPhoneDetails}
+          {...phone}
+        />)}
+      </section>
+
+      <Modal isOpen={modalIsOpen} toggle={this.toggleModal}>
+        <ModalHeader toggle={this.toggleModal}>{selectedDeviceName}</ModalHeader>
+        <ModalBody>
+
+        </ModalBody>
+      </Modal>
+    </Fragment>);
   }
 }
 
